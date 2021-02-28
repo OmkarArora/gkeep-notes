8f124096e7659ebde2fbfa0ef539555bf5b66170
Omkar Arora <aroraomkar@gmail.com>
2021-02-28 18:47:57 +0530

    style: input styles

--- src/components/NotesSection/NotesInput/NotesInput.jsx
+++ src/components/NotesSection/NotesInput/NotesInput.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import NotesMenu from "../NotesMenu/NotesMenu";
 import TagsDisplay from "../TagsDisplay/TagsDisplay";
+import { AiOutlinePushpin, AiFillPushpin } from "react-icons/ai";
 
-import "./notesInput.css"
+import "./notesInput.css";
 
 const NotesInput = ({ addNewNote }) => {
   const [titleState, setTitle] = useState("");
   const [contentState, setContent] = useState("");
   const [bgColor, setBgColor] = useState("");
   const [addedTags, addTag] = useState([]);
+  const [isPinned, setPin] = useState(false);
 
   const onClickAdd = () => {
     addNewNote({ title: titleState, content: contentState });
@@ -28,33 +30,62 @@ const NotesInput = ({ addNewNote }) => {
   };
 
   const deleteTag = (tag) => {
-    let updatedTags = addedTags.filter(item => tag.toLowerCase()!==item.toLowerCase())
+    let updatedTags = addedTags.filter(
+      (item) => tag.toLowerCase() !== item.toLowerCase()
+    );
     addTag(updatedTags);
-  }
+  };
+
+  const updateInput = (event, type) => {
+    event.target.style.height = "inherit";
+    event.target.style.height = `${event.target.scrollHeight}px`;
+    if (type === "title") setTitle(event.target.value);
+    else setContent(event.target.value);
+  };
 
   return (
-    <div className="container-notesInput" style={{ backgroundColor: bgColor === "" ? "" : `var(${bgColor})` }}>
-      <div>
-        <input
+    <div
+      className="container-notesInput"
+      style={{
+        backgroundColor: bgColor === "" ? "" : `var(${bgColor})`,
+        borderColor:
+          bgColor === "" || bgColor.includes("brown")
+            ? `var(--color-light-gray)`
+            : `var(${bgColor})`,
+      }}
+    >
+      <div className="container-input-topRow">
+        <textarea
+          rows="1"
+          className="input-title"
           placeholder="Title"
           value={titleState}
-          onChange={(event) => setTitle(event.target.value)}
-        ></input>
-      </div>
-      <div>
-        <textarea
-          placeholder="Take a note..."
-          value={contentState}
-          onChange={(event) => setContent(event.target.value)}
+          onChange={(event) => updateInput(event, "title")}
         ></textarea>
+        <div className="container-pin" onClick={()=>setPin(!isPinned)}>
+          {isPinned ? <AiFillPushpin /> : <AiOutlinePushpin />}
+        </div>
       </div>
+
+      <textarea
+        rows="1"
+        className="input-content"
+        placeholder="Take a note..."
+        value={contentState}
+        onChange={(event) => updateInput(event, "content")}
+      ></textarea>
+
       <div>
-        <TagsDisplay tags={addedTags} tagsId={"newNote"} deleteTag={(tag)=>deleteTag(tag)}/>
+        <TagsDisplay
+          tags={addedTags}
+          tagsId={"newNote"}
+          deleteTag={(tag) => deleteTag(tag)}
+        />
       </div>
       <NotesMenu
         updateBgColor={(color) => updateBgColor(color)}
         activeColor={bgColor}
-        updateTagsList={(tag)=>updateTagsList(tag)}
+        updateTagsList={(tag) => updateTagsList(tag)}
       />
       <button onClick={onClickAdd}>Add</button>
     </div>
