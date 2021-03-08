36ffe770d80edc7d64f435f1d1308d85442f17c0
Omkar Arora <aroraomkar@gmail.com>
2021-03-08 19:52:52 +0530

    feat: sidenav hover effects

--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -3,10 +3,24 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import "./sidenav.css";
 
 export const Sidenav = () => {
-	return (
-		<div className="sidenav">
-			<AiOutlineBulb className="icon icon-bulb"/>
-			<RiDeleteBin6Line className="icon icon-bin"/>
-		</div>
-	)
-}
+  return (
+    <div className="sidenav">
+      <nav class="navbar">
+        <ul className="navbar-nav">
+          <li class="nav-item">
+            <a href="https://www.google.com/" class="nav-link">
+              <AiOutlineBulb className="icon icon-bulb" />
+			  <span className="link-text">Notes</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://www.google.com/" class="nav-link">
+              <RiDeleteBin6Line className="icon icon-bin" />
+			  <span className="link-text">Trash</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
