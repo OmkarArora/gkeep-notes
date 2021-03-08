fe288b020ff6227b692a9cedaaaad4321cb48753
Omkar Arora <aroraomkar@gmail.com>
2021-03-08 20:08:23 +0530

    feat: pinned and other notes segregated in display

--- src/components/NotesSection/NotesDisplay/NotesDisplay.jsx
+++ src/components/NotesSection/NotesDisplay/NotesDisplay.jsx
@@ -3,14 +3,59 @@ import NotesCard from "./NotesCard/NotesCard";
 import "./notesDisplay.css";
 
 const NotesDisplay = ({ notes, updateDisplayNote }) => {
-  return (
+  const pinnedNotes = notes.filter((note) => note.isPinned);
+  const otherNotes = notes.filter((note) => !note.isPinned);
+  return pinnedNotes.length === 0 ? (
     <div className="container-notes">
       {notes.map((item) => (
-        <NotesCard key={`Note${item.id}`} tagId={item.id} title={item.title} content={item.content} bgColor={item.bgColor} tags={item.tags} isPinned={item.isPinned} 
-        updateDisplayNote=
-        {(id,property,value)=>updateDisplayNote(id,property,value)}
+        <NotesCard
+          key={`Note${item.id}`}
+          tagId={item.id}
+          title={item.title}
+          content={item.content}
+          bgColor={item.bgColor}
+          tags={item.tags}
+          isPinned={item.isPinned}
+          updateDisplayNote={(id, property, value) =>
+            updateDisplayNote(id, property, value)
+          }
         />
       ))}
+    </div>
+  ) : (
+    <div>
+      <div className="section-pinned-notes container-notes">
+        {pinnedNotes.map((item) => (
+          <NotesCard
+            key={`Note${item.id}`}
+            tagId={item.id}
+            title={item.title}
+            content={item.content}
+            bgColor={item.bgColor}
+            tags={item.tags}
+            isPinned={item.isPinned}
+            updateDisplayNote={(id, property, value) =>
+              updateDisplayNote(id, property, value)
+            }
+          />
+        ))}
+      </div>
+      <div className="section-other-notes container-notes">
+        {otherNotes.map((item) => (
+          <NotesCard
+            key={`Note${item.id}`}
+            tagId={item.id}
+            title={item.title}
+            content={item.content}
+            bgColor={item.bgColor}
+            tags={item.tags}
+            isPinned={item.isPinned}
+            updateDisplayNote={(id, property, value) =>
+              updateDisplayNote(id, property, value)
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -5,16 +5,16 @@ import "./sidenav.css";
 export const Sidenav = () => {
   return (
     <div className="sidenav">
-      <nav class="navbar">
+      <nav className="navbar">
         <ul className="navbar-nav">
-          <li class="nav-item">
-            <a href="https://www.google.com/" class="nav-link">
+          <li className="nav-item">
+            <a href="https://www.google.com/" className="nav-link">
               <AiOutlineBulb className="icon icon-bulb" />
 			  <span className="link-text">Notes</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="https://www.google.com/" class="nav-link">
+          <li className="nav-item">
+            <a href="https://www.google.com/" className="nav-link">
               <RiDeleteBin6Line className="icon icon-bin" />
 			  <span className="link-text">Trash</span>
             </a>
